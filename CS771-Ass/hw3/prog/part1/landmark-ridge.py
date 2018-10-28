7be82490af024b842b2ff943b0ebcb1a293d8b0b
Shashi Kant <shashikg.iitk@gmail.com>
2018-10-28 16:31:45 +0530

    hw3 landmark ridge done

--- CS771-Ass/hw3/prog/part1/landmark-ridge.py
+++ CS771-Ass/hw3/prog/part1/landmark-ridge.py
@@ -8,25 +8,30 @@ def load_data(loc):
     test = np.genfromtxt(loc+'/ridgetest.txt',delimiter='  ')
     return train[:,0], train[:,1], test[:,0], test[:,1]
 
-def kernel(x, y):
-    # K = kernel(x_train, x_train)
+def landmark(x, y):
+    #  = kernel(x_train, x_train)
     return np.exp(-0.1*np.square(x.reshape((-1,1)) - y.reshape((1,-1))))
 
 x_train, y_train, x_test, y_test = load_data('data')
-K = kernel(x_train, x_train)
-iter = [0, 0.1, 1, 10, 100]
-In = np.eye(x_train.shape[0])
 
-for lam in iter:
-    alpha = np.dot(np.linalg.inv(K + lam*In), y_train.reshape((-1,1)))
-    K_test = kernel(x_train, x_test)
-    y_pred = (np.dot(alpha.T, K_test)).reshape((-1,1))
+iter = [2, 5, 20, 50, 100]
+
+for L in iter:
+    z = np.random.choice(x_train, L, replace=False)
+    Id = np.eye(L)
+    xf_train = landmark(x_train, z)
+
+    W = np.dot(np.linalg.inv(np.dot(xf_train.T, xf_train) + 0.1*Id), np.dot(xf_train.T, y_train.reshape((-1,1))))
+
+    xf_test = landmark(x_test, z)
+
+    y_pred = np.dot(xf_test, W)
 
     rmse = np.sqrt(np.mean(np.square(y_test.reshape((-1,1)) - y_pred)))
-    print 'RMSE for lambda = ' + str(lam) + ' is ' + str(rmse)
+    print 'RMSE for lambda = ' + str(L) + ' is ' + str(rmse)
 
-    plt.figure(lam)
-    plt.title('lambda = ' + str(lam) + ', rmse = ' + str(rmse))
+    plt.figure(L)
+    plt.title('L = ' + str(L) + ', rmse = ' + str(rmse))
     plt.plot(x_test, y_pred, 'r*')
     plt.plot(x_test, y_test, 'b*')
 
